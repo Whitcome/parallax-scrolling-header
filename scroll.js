25faa5905ee9c25fbd2967932ccf97407b8a1b1b
bogdanfer <ferariu_boby@yahoo.com>
2017-08-06 01:26:45 +0200

    Removed jQuery dependency, added CSS optimisation

--- scroll.js
+++ scroll.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-    var moving__background = $("#header__background");
-    $(window).scroll(function() { 
-        moving__background.css('margin-top', ($(window).scrollTop())/3); // Parallax scrolling
-        moving__background.css('opacity', 1 - ($(window).scrollTop())/moving__background.height()); // Fading out
+document.addEventListener('DOMContentLoaded', function() {
+    const moving__background = document.querySelector('#header__background');
+
+    window.addEventListener( 'scroll', function() {
+        ParallaxAnimation( moving__background );
     });
 });
+
+function ParallaxAnimation( el ) {
+    let scrollTop = document.body.scrollTop;
+
+    el.style.marginTop = (scrollTop / 3) + 'px';
+    el.style.opacity = (1 - scrollTop / el.offsetHeight );
+};
