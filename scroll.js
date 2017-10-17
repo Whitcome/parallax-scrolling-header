2e9116f031860dd754095da3ba79c4e3d189e380
Whit128 <Whit128@users.noreply.github.com>
2017-10-17 11:26:22 -0500

    Seperated the header image and text from the header
    This will make them scroll with the page rather than the header.
    
    Fade out was also turned off by default, but the js code is left commented in just in case.

--- scroll.js
+++ scroll.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function ParallaxAnimation(el) {
-    el.css('margin-top', ($(window).scrollTop())/3); // Parallax scrolling
-    el.css('opacity', 1 - ($(window).scrollTop())/el.height()); // Fading out
+    el.css('margin-top', ($(window).scrollTop())/3); // Parallax scrolling. Increasing the denominator will make the parallax effect less pronounced.
+
+    // Code below is for fading out. I left it in just in case you want to use it.
+    //el.css('opacity', 1 - ($(window).scrollTop())/el.height()); 
 };
